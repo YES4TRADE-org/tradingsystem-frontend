19742effaab132dde553ca862fd41af1950b02d1
Raphae San Juan <raphaelsanjuan6@gmail.com>
2025-09-23 00:48:13 +0800

    i didnt anything still debugging will fix tomorrow

--- my-app/src/app/components/Home.jsx
+++ my-app/src/app/components/Home.jsx
@@ -4,17 +4,17 @@ import { useState, useEffect } from "react";
 
 export default function Books(){
     const [books, setBooks] = useState([]);
-    
+
     useEffect(() => {
-        fetch('http://localhost:5000/yes4trade/getbooks')
-        .then(res => {
-            if(!res.ok){
-                throw new Error('Error! Cannot get books');
+        fetch("http://localhost:5000/yes4trade/getbooks")
+        .then((res) => {
+            if (!res.ok) {
+            throw new Error("Error! Cannot get books");
             }
             return res.json();
         })
-        .then(json => {
-            console.log(json);
+        .then((json) => {
+            console.log("Fetched:", json);
             setBooks(json);
         })
         .catch((err) => {
@@ -22,13 +22,18 @@ export default function Books(){
         });
     }, []);
 
-    console.log(books);
-
     return (
         <>
-          <ul>
-            {books.map((book) => (<li key={book.books_id}>{book.title}</li>))}
-          </ul>
+          <div className="inline-block flex space-x-10 p-5 border">
+                {books.map((book) => 
+                (<section className="inline-block flex space-x-10 p-20 m-10
+                                border" key={book.books_id}>
+                    <div>
+                        <img className="w-20 h-20" src={`data:image/png;base64,${book.image}`} alt={book.title}></img>
+                        <h1>{book.title}</h1>
+                    </div>
+                </section>))}
+          </div>  
         </>
     );
 }
