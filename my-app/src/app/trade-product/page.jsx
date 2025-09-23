87b022edae301beb9bfe263cdbc8922af3798111
Raphae San Juan <raphaelsanjuan6@gmail.com>
2025-09-23 23:02:06 +0800

    add an input for student id

--- my-app/src/app/trade-product/page.jsx
+++ my-app/src/app/trade-product/page.jsx
@@ -4,11 +4,13 @@ export default function Trade(){
           <section>
             <div className="m-10 px-10 rounded-lg font-mono">
                 <h1 className="text-3xl mb-4 font-bold">Email: </h1>
-                <input className="border-b w-100 border-white-500 rounded-sm" type='email' placeholder='Enter your email'></input>
+                <input className="outline-none border-0 border-b w-100 border-white-500 rounded-sm"
+                    type='email' placeholder='Enter your email' required></input>
             </div>
             <div className="m-10 px-10 rounded-lg font-mono">
-                <h1 className="text-3xl mb-4 font-bold">Email: </h1>
-                <input className="border-0 border-b w-100 border-white-500 rounded-sm" type='email' placeholder='Enter your email'></input>
+                <h1 className="text-3xl mb-4 font-bold">Student ID: </h1>
+                <input className="outline-none border-0 border-b w-100 border-white-500 rounded-sm"
+                    placeholder='Enter your student id' required></input>
             </div>
           </section>
         </>
