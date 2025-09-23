0a3c8ee672ff57d3edf5a5e7b3dc5f74e92ebeae
Raphae San Juan <raphaelsanjuan6@gmail.com>
2025-09-23 23:07:28 +0800

    created a grid column to seperate two sections

--- my-app/src/app/trade-product/page.jsx
+++ my-app/src/app/trade-product/page.jsx
@@ -1,18 +1,42 @@
 export default function Trade(){
     return (
         <>
-          <section>
-            <div className="m-10 px-10 rounded-lg font-mono">
-                <h1 className="text-3xl mb-4 font-bold">Email: </h1>
-                <input className="outline-none border-0 border-b w-100 border-white-500 rounded-sm"
-                    type='email' placeholder='Enter your email' required></input>
-            </div>
-            <div className="m-10 px-10 rounded-lg font-mono">
-                <h1 className="text-3xl mb-4 font-bold">Student ID: </h1>
-                <input className="outline-none border-0 border-b w-100 border-white-500 rounded-sm"
-                    placeholder='Enter your student id' required></input>
-            </div>
-          </section>
+            <main className="grid grid-cols-2 gap-1 m-10">
+                <section>
+                    <div className="m-15 px-10 rounded-lg font-mono">
+                        <h1 className="text-3xl mb-4 font-bold">Email: </h1>
+                        <input className="outline-none border-0 border-b w-100 border-white-500 rounded-sm"
+                            type='email' placeholder='Enter your email' required></input>
+                    </div>
+                    <div className="m-15 px-10 rounded-lg font-mono">
+                        <h1 className="text-3xl mb-4 font-bold">Student ID: </h1>
+                        <input className="outline-none border-0 border-b w-100 border-white-500 rounded-sm"
+                            placeholder='Enter your student id' required></input>
+                    </div>
+                    <div className="m-15 px-10 rounded-lg font-mono">
+                        <h1 className="text-3xl mb-4 font-bold">Student ID: </h1>
+                        <input className="outline-none border-0 border-b w-100 border-white-500 rounded-sm"
+                            placeholder='Enter your student id' required></input>
+                    </div>
+                </section>
+                <section>
+                    <div className="m-15 px-10 rounded-lg font-mono">
+                        <h1 className="text-3xl mb-4 font-bold">Email: </h1>
+                        <input className="outline-none border-0 border-b w-100 border-white-500 rounded-sm"
+                            type='email' placeholder='Enter your email' required></input>
+                    </div>
+                    <div className="m-15 px-10 rounded-lg font-mono">
+                        <h1 className="text-3xl mb-4 font-bold">Student ID: </h1>
+                        <input className="outline-none border-0 border-b w-100 border-white-500 rounded-sm"
+                            placeholder='Enter your student id' required></input>
+                    </div>
+                    <div className="m-15 px-10 rounded-lg font-mono">
+                        <h1 className="text-3xl mb-4 font-bold">Student ID: </h1>
+                        <input className="outline-none border-0 border-b w-100 border-white-500 rounded-sm"
+                            placeholder='Enter your student id' required></input>
+                    </div>
+                </section>
+            </main>
         </>
     );
 }
