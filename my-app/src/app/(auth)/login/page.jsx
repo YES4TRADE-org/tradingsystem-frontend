069211a4e2e8409a69b93d3927ddd91ef728fde2
Raphae San Juan <raphaelsanjuan6@gmail.com>
2025-09-27 11:49:29 +0800

    move all folders and add an auth folder for solo layout

---
+++ my-app/src/app/(auth)/login/page.jsx
@@ -0,0 +1,9 @@
+export default function Login(){
+    return <>
+        <section className="border rounded-lg border-red-900 w-100 h-100">
+            <div>
+            <h1 className="text-3xl">Hello</h1>
+            </div>
+        </section>
+    </>
+}
