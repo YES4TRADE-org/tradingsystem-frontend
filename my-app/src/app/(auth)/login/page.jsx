d1802d44839648f1914b4a8e2ac80645f37a2742
Raphae San Juan <raphaelsanjuan6@gmail.com>
2025-09-27 11:52:35 +0800

    seperate the login and the image

--- my-app/src/app/(auth)/login/page.jsx
+++ my-app/src/app/(auth)/login/page.jsx
@@ -1,8 +1,11 @@
 export default function Login(){
     return <>
-        <section className="border rounded-lg border-red-900 w-100 h-100">
+        <section className="border rounded-lg border-red-900 w-250 h-150 grid grid-cols-2 gap-1">
             <div>
-            <h1 className="text-3xl">Hello</h1>
+                <h1 className="text-3xl">Hello</h1>
+            </div>
+            <div>
+                <h1 className="text-3xl">Hi</h1>
             </div>
         </section>
     </>
